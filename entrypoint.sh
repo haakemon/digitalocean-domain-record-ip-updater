c3ff8a15c74cab1cff7f87c8d5225311a4494630
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2022-01-20 21:56:45 +0100

    fix: create cron log file, add correct cron time

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,11 @@
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
+touch /var/log/cron.log
 
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-* * * * * /usr/local/bin/node /app/dist/index.js >> /var/log/cron.log 2>&1
+* 3 * * * /usr/local/bin/node /app/dist/index.js >> /var/log/cron.log 2>&1
 " > domain_record_cron
 
 crontab domain_record_cron
