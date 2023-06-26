867c1af7db390606ae97289094edeeab9109c46e
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-06-26 14:47:11 +0200

    feat: execute check once when initialized

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 
 touch /var/log/cron.log
 
+/usr/local/bin/node /app/dist/index.js
+
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
 0 3 * * * /usr/local/bin/node /app/dist/index.js >> /var/log/cron.log 2>&1
